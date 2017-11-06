187f490c789c56c691758c85a6ac2f262db25dbf
Nick Ebbutt <nick@ebbutt.net>
2017-11-06 17:00:47 +0000

    Chorus integration with basic React app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        myText : 'Nick'
+    }
+  }
+    
+  setName(newName) {
+      this.setState({ myText: newName })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React {this.state.myText}</h1>
         </header>
         <p className="App-intro">
           To get started Nick, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import client, { clientOpened } from './chorusClient';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppComponent = ReactDOM.render(<App />, document.getElementById('root'));
+
+clientOpened.then(() => {
+    console.log('Chorus Connection Opened!');
+
+    client.publishStep('.*say hello to the console', () => { console.log("Hello"); });
+
+    client.publishStep('.*set the name to (.*)', ([name]) => { AppComponent.setName(name)});
+
+    client.stepsAligned();
+});
+
 registerServiceWorker();
+
+
