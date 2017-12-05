dd2dc198a2c7e835807f2ab71d702217a05289c8
Nick Ebbutt <nick@ebbutt.net>
2017-12-05 17:16:44 +0000

    Add a simple chorus feature

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { CalculatorContainer } from './calculator/CalculatorContainer';
 import { reducer } from './calculator/CalculatorDuck';
+import expect from 'expect';
+
 
 clientOpened.then(() => {
     console.log('Chorus Connection Opened!');
@@ -13,6 +15,16 @@ clientOpened.then(() => {
     client.publishStep('.*say hello to the console', () => { console.log("Hello"); });
 
     client.publishStep('.*set the name to (.*)', ([name]) => { AppComponent.setName(name)});
+    
+    client.publishStep('.*press the (.*) key', ([key]) => { 
+        const button = document.querySelector('.key-' + key);
+        button.click();
+    });
+
+    client.publishStep('.*the display shows (.*)', ([value]) => {
+        const displayDiv = document.querySelector('.auto-scaling-text');
+        expect(displayDiv.textContent).toBe(value)
+    });
 
     client.stepsAligned();
 });
