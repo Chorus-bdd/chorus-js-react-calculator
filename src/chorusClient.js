2f45a5079ba49195830a7e09881b62826a021a60
Nick Ebbutt <nick@ebbutt.net>
2018-02-19 10:18:46 +0000

    Do not create chorus client if http params are not set

--- src/chorusClient.js
+++ src/chorusClient.js
@@ -4,10 +4,11 @@ import createClient from 'chorus-js';
 const client = createClient('CalculatorStepPublisher', 'A simple chorus step publisher');
 export default client;
 
-var urlParams = new URLSearchParams(window.location.search);
-var chorusHostAndPort = urlParams.get('chorusHostAndPort');
+const urlParams = new URLSearchParams(window.location.search);
+const chorusHostAndPort = urlParams.get('chorusHostAndPort');
 
-export const clientOpened =
-	client
-		.open('ws://' + chorusHostAndPort)
-		.then(() => client.connect());
+export const clientOpened = chorusHostAndPort ?
+    client
+        .open(`ws://${chorusHostAndPort}`)
+        .then(() => client.connect()) :
+    new Promise(() => {});
