e61b1b88e94ab221b83abcde5c854a9888e779b7
Nick Ebbutt <nick@ebbutt.net>
2018-03-01 21:51:33 +0000

    Update to use latest chorus-interpreter image

--- e2e/e2e.sh
+++ e2e/e2e.sh
@@ -6,13 +6,6 @@ cleanup() {
     docker-compose stop
 }
 
-echo "Pulling Docker images.."
-docker pull chorusbdd/chorus-interpreter:3.0.0-DEV32
-docker pull selenium/hub:3.9.1
-docker pull selenium/node-chrome-debug:3.9.1
-#docker pull selenium/node-firefox-debug:3.9.1
-docker pull node:8.9.1
-
 echo "Starting Docker services..."
 docker-compose up -d
 if [ $? -ne 0 ] ; then
